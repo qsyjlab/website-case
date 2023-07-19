a9a091635f7c95eb037068c470a8da1d87b133ce
青衫依旧 <2461521018@qq.com>
2023-07-19 10:49:05 +0800

    feat: update dist

--- website/src/utils/fetch.js
+++ website/src/utils/fetch.js
@@ -2,7 +2,7 @@
 
 import jsYaml from "js-yaml";
 
-export const baseUrl = "/website/";
+export const baseUrl = "/website/system";
 
 export function fetchYml(path) {
   return new Promise((resolve, reject) => {
