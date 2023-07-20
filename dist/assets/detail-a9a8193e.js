8e81c20120473fc23b6096a5bf7e4363ab44d222
青衫依旧 <2461521018@qq.com>
2023-07-20 09:50:52 +0800

    feat: update dist

---
+++ dist/assets/detail-a9a8193e.js
@@ -0,0 +1,1 @@
+import{_ as a,g as t,o,c as _,b as r,u as e,n as c}from"./index-16e499d0.js";import{e as n}from"./js-yaml-4f9bef47.js";const l={class:"iframe-cotainer"},i=["src"],u={__name:"detail",setup(p){const s=t().query;return(d,f)=>(o(),_("div",l,[r("iframe",{src:e(n)+e(s).path,class:c("iframe-page__main")},null,8,i)]))}},v=a(u,[["__scopeId","data-v-b1e3e00f"]]);export{v as default};
--- website/src/views/home/case-list.vue
+++ website/src/views/home/case-list.vue
@@ -4,18 +4,37 @@
       class="case-wrapper__item"
       v-for="(item, index) in caseList"
       :key="index"
+      @click="$router.push({ name: 'View', query: { path: item.folder } })"
     >
-      <router-link :to="{ name: 'View', query: { path: item.folder } }">{{
-        item.name
-      }}</router-link>
+      <div class="iframe-wrapper">
+        <div class="iframe-wrapper__mask"></div>
+        <iframe
+          :data-src="examplesUrl + item.folder"
+
+          :class="`iframe-page__main iframe-lazy`"
+        ></iframe>
+      </div>
+      <div class="case-wrapper__title">{{ item.name }}</div>
     </div>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
-import { fetchYml } from "@/utils";
+import { nextTick, ref } from "vue";
+import { fetchYml, examplesUrl } from "@/utils";
 
 const caseList = ref([]);
+
+// 创建 Intersection Observer 实例
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    // 当目标元素进入视口时，触发加载图片
+    if (entry.isIntersecting) {
+      const lazyImage = entry.target;
+      lazyImage.src = lazyImage.dataset.src;
+      observer.unobserve(lazyImage); // 停止观察已加载的图片
+    }
+  });
+});
 
 startLoadSystem();
 
@@ -35,17 +54,62 @@ function startLoadSystem() {
 
     caseList.value = foldersData.flat();
 
-    console.log("caseList", caseList);
+    nextTick(() => {
+      // 获取所有带有 'lazy-load' 类的图片元素
+      const lazys = document.querySelectorAll(".iframe-lazy");
+      // 将所有图片元素添加到 Intersection Observer 中进行观察
+      lazys.forEach((lazy) => {
+        observer.observe(lazy);
+      });
+    });
+
   });
 }
 </script>
 <style lang="scss" scoped>
+.iframe-wrapper {
+  position: relative;
+  width: 100%;
+  padding-top: 56.25%; /* 16:9 宽高比，(9 / 16) * 100% = 56.25% */
+  // background-color: #f0f0f0;
+  background-color: white;
+  &__mask {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 2;
+    cursor: pointer;
+  }
+}
 .case-wrapper {
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px; /* 列之间的间隔 */
 
   &__item {
+    // width: 24%;
+    box-sizing: border-box;
     margin: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.18);
   }
+
+  &__title {
+    padding: 5px;
+  }
+}
+.iframe-page__main {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow: hidden;
+  border: 0;
 }
 </style>
